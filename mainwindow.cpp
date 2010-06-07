1b56c61a62093c2ef2b323cf4c9d72593fde9f41
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-06-07 20:46:36 +0300

    Load QML on startup

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,11 @@
 #include "mainwindow.h"
 
+#include <QUrl>
+
 MainWindow::MainWindow(QWidget *parent) :
     QDeclarativeView(parent)
 {
-
+    setSource(QUrl("qrc:/shoppinglist/qml/main.qml"));
 }
 
 MainWindow::~MainWindow()
