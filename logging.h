d5238e521b687bb961be25d13373eb3e2a43fe7b
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2010-06-08 14:10:19 +0300

    Fancier logging macros

--- logging.h
+++ logging.h
@@ -3,14 +3,20 @@
 
 #include <QDebug>
 
-#ifndef NDEBUG
-# define LOG_DEBUG(msg) qDebug() << "[Debug]" << FORMAT(msg);
-# define LOG_WARNING(msg) qWarning() << "[WARNING]" << FORMAT(msg);
-# define LOG_ERROR(msg) qCritical() << "[ERROR]" << FORMAT(msg);
+#ifndef QT_NO_DEBUG_OUTPUT
+# define FORMAT(msg) __FUNCTION__ << " (" << __FILE__ << ":" << __LINE__ << ") " << msg
+# define LOG_DEBUG(msg) qDebug().nospace() << "[Debug] " << FORMAT(msg)
+# define LOG_WARNING(msg) qWarning().nospace() << "[WARNING] " << FORMAT(msg)
+# define LOG_ERROR(msg) qCritical().nospace() << "[ERROR] " << FORMAT(msg)
+int log_indentlevel = 0;
+# define LOG_ENTRY() do { qDebug().nospace() << "-- " << __FUNCTION__ << " entered"; log_indentlevel++; } while(0)
+# define LOG_EXIT() do { qDebug().nospace() << "-- " << __FUNCTION__ << " exited"; log_indentlevel--; } while(0)
 #else
 # define LOG_DEBUG(msg)
 # define LOG_WARNING(msg)
 # define LOG_ERROR(msg)
+# define LOG_ENTRY()
+# define LOG_EXIT()
 #endif
 
 #endif // LOGGING_H
