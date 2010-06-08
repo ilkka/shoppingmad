7fc8c92ac69e24639125d771130b3231f2146cb2
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2010-06-08 13:23:01 +0300

    Use C++ model in QML ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 
+#include "wantedmodel.h"
+
 #include <QUrl>
+#include <QDeclarativeContext>
 
 MainWindow::MainWindow(QWidget *parent) :
     QDeclarativeView(parent)
 {
     setSource(QUrl("qrc:/shoppinglist/qml/main.qml"));
+    rootContext()->setContextProperty("WantedItemsModel", new WantedModel);
 }
 
 MainWindow::~MainWindow()
