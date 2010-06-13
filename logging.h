47bd460b895c11f4f96047bf015605dbc9f9a981
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-06-13 13:32:46 +0300

    Remove indentlevel stuff from logging macros

--- logging.h
+++ logging.h
@@ -8,9 +8,8 @@
 # define LOG_DEBUG(msg) qDebug().nospace() << "[Debug] " << FORMAT(msg)
 # define LOG_WARNING(msg) qWarning().nospace() << "[WARNING] " << FORMAT(msg)
 # define LOG_ERROR(msg) qCritical().nospace() << "[ERROR] " << FORMAT(msg)
-int log_indentlevel = 0;
-# define LOG_ENTRY() do { qDebug().nospace() << "-- " << __FUNCTION__ << " entered"; log_indentlevel++; } while(0)
-# define LOG_EXIT() do { qDebug().nospace() << "-- " << __FUNCTION__ << " exited"; log_indentlevel--; } while(0)
+# define LOG_ENTRY() do { qDebug().nospace() << "-- " << __FUNCTION__ << " entered"; } while(0)
+# define LOG_EXIT() do { qDebug().nospace() << "-- " << __FUNCTION__ << " exited"; } while(0)
 #else
 # define LOG_DEBUG(msg)
 # define LOG_WARNING(msg)
