859a6da5d5912fe1402a938cddd6de85e56b79d5
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-06-13 13:25:08 +0300

    Add indentation levels to logging macros

--- logging.h
+++ logging.h
@@ -5,9 +5,9 @@
 
 #ifndef QT_NO_DEBUG_OUTPUT
 # define FORMAT(msg) __FUNCTION__ << " (" << __FILE__ << ":" << __LINE__ << ") " << msg
-# define LOG_DEBUG(msg) qDebug().nospace() << "[Debug] " << FORMAT(msg)
-# define LOG_WARNING(msg) qWarning().nospace() << "[WARNING] " << FORMAT(msg)
-# define LOG_ERROR(msg) qCritical().nospace() << "[ERROR] " << FORMAT(msg)
+# define LOG_DEBUG(msg) qDebug().nospace() << "[Debug]" << QString("%1").arg(" ", log_indentlevel, " ") << FORMAT(msg)
+# define LOG_WARNING(msg) qWarning().nospace() << "[WARNING]" << QString("%1").arg(" ", log_indentlevel, " ")  << FORMAT(msg)
+# define LOG_ERROR(msg) qCritical().nospace() << "[ERROR]" << QString("%1").arg(" ", log_indentlevel, " ")  << FORMAT(msg)
 int log_indentlevel = 0;
 # define LOG_ENTRY() do { qDebug().nospace() << "-- " << __FUNCTION__ << " entered"; log_indentlevel++; } while(0)
 # define LOG_EXIT() do { qDebug().nospace() << "-- " << __FUNCTION__ << " exited"; log_indentlevel--; } while(0)
