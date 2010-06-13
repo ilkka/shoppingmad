33886eb8ab5e7692b734101356cbabb56f5ac4a9
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-06-13 13:37:19 +0300

    Add trace level macro

--- logging.h
+++ logging.h
@@ -5,12 +5,14 @@
 
 #ifndef QT_NO_DEBUG_OUTPUT
 # define FORMAT(msg) __FUNCTION__ << " (" << __FILE__ << ":" << __LINE__ << ") " << msg
+# define LOG_TRACE(msg) qDebug().nospace() << "[Trace] " << FORMAT(msg)
 # define LOG_DEBUG(msg) qDebug().nospace() << "[Debug] " << FORMAT(msg)
 # define LOG_WARNING(msg) qWarning().nospace() << "[WARNING] " << FORMAT(msg)
 # define LOG_ERROR(msg) qCritical().nospace() << "[ERROR] " << FORMAT(msg)
 # define LOG_ENTRY() do { qDebug().nospace() << "-- " << __FUNCTION__ << " entered"; } while(0)
 # define LOG_EXIT() do { qDebug().nospace() << "-- " << __FUNCTION__ << " exited"; } while(0)
 #else
+# define LOG_TRACE(msg)
 # define LOG_DEBUG(msg)
 # define LOG_WARNING(msg)
 # define LOG_ERROR(msg)
